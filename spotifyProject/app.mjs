5dd7a5b07352a88f09600c853a8f63f93e3002a8
Tyler-Culp <tculp@ucsd.edu>
2023-09-15 15:31:26 -0700

    Hopefully fixed only one user allowed issue

--- spotifyProject/app.mjs
+++ spotifyProject/app.mjs
@@ -1,13 +1,10 @@
 import express from 'express';
-import http from 'http';
 import querystring from 'querystring';const app = express();
 const PORT = process.env.PORT || 3000;
 import fetch from 'node-fetch';
 import { CLIENT_ID, CLIENT_SECRET, REDIRECT_URI } from './config.mjs';
 
 const SPOTIFY_API_BASE = 'https://api.spotify.com/v1';
-
-let accessToken = '';
 
 app.get('/login', (req, res) => {
     console.log("login page reached");
@@ -26,12 +23,6 @@ app.get('/login', (req, res) => {
   app.get('/callback', async (req, res) => {
     console.log("callback page reached");
     const { code, state } = req.query;
-    // Verify the state parameter to prevent CSRF attacks
-    /*
-    if (state !== req.session.state) {
-      return res.status(400).send('Invalid state parameter');
-    }
-    */
     try {
       // Exchange the authorization code for an access token
       const tokenResponse = await fetch('https://accounts.spotify.com/api/token', {
@@ -52,41 +43,41 @@ app.get('/login', (req, res) => {
       }
   
       const tokenData = await tokenResponse.json();
-      accessToken = tokenData.access_token;
+      let accessToken = tokenData.access_token;
   
       // Redirect to a new URL where the user's top songs are displayed
-      res.redirect('./top-songs');
+      res.send(`<h1>Your Top Songs on Spotify</h1><pre>${JSON.stringify(topSongs, null, 2)}</pre>`);
     } catch (error) {
       console.error('Error:', error.message);
       res.status(500).send('Error obtaining access token');
     }
   });
   
-  app.get('/top-songs', async (req, res) => {
-    console.log("Top songs reached");
-    try {
-      // Fetch the user's top songs using the access token
-      const response = await fetch(`${SPOTIFY_API_BASE}/me/top/tracks`, {
-        headers: {
-          'Authorization': `Bearer ${accessToken}`,
-        },
-      });
+  // app.get('/top-songs', async (req, res) => {
+  //   console.log("Top songs reached");
+  //   try {
+  //     // Fetch the user's top songs using the access token
+  //     const response = await fetch(`${SPOTIFY_API_BASE}/me/top/tracks`, {
+  //       headers: {
+  //         'Authorization': `Bearer ${accessToken}`,
+  //       },
+  //     });
   
-      if (!response.ok) {
-        throw new Error('Top songs request failed');
-      }
+  //     if (!response.ok) {
+  //       throw new Error('Top songs request failed');
+  //     }
   
-      const topSongs = await response.json();
-      console.log(`top songs json = ${topSongs}`);
+  //     const topSongs = await response.json();
+  //     console.log(`top songs json = ${topSongs}`);
   
-      // Display the user's top songs
-      res.send(`<h1>Your Top Songs on Spotify</h1><pre>${JSON.stringify(topSongs, null, 2)}</pre>`);
-    } catch (error) {
-      console.error('Error:', error.message);
-      res.status(500).send('Error fetching top songs');
-    }
-  });
+  //     // Display the user's top songs
+  //     res.send(`<h1>Your Top Songs on Spotify</h1><pre>${JSON.stringify(topSongs, null, 2)}</pre>`);
+  //   } catch (error) {
+  //     console.error('Error:', error.message);
+  //     res.status(500).send('Error fetching top songs');
+  //   }
+  // });
   
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
+  // app.listen(PORT, () => {
+  //   console.log(`Server is running on port ${PORT}`);
+  // });
