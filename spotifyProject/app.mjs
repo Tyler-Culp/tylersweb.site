a1af5e638c0709278d1a9d36c46b54a67dd688b6
Tyler <tylerculp2002@gmail.com>
2023-09-15 22:18:31 +0000

    Added updated spotify code and node stuff

--- spotifyProject/app.mjs
+++ spotifyProject/app.mjs
@@ -1,40 +1,37 @@
-const express = require('express');
-const http = require('http');
-const querystring = require('querystring');
-const app = express();
+import express from 'express';
+import http from 'http';
+import querystring from 'querystring';const app = express();
 const PORT = process.env.PORT || 3000;
+import fetch from 'node-fetch';
+import { CLIENT_ID, CLIENT_SECRET, REDIRECT_URI } from './config.mjs';
 
-const CLIENT_ID = process.env.CLIENT_ID;
-const CLIENT_SECRET = process.env.CLIENT_SECRET;
-const REDIRECT_URI = 'https://tylersweb.site/spotifyProject/callback'; // Update with your actual redirect URI
 const SPOTIFY_API_BASE = 'https://api.spotify.com/v1';
 
 let accessToken = '';
 
 app.get('/login', (req, res) => {
+    console.log("login page reached");
     const scope = 'user-top-read'; // Specify required scope for top songs
   
     // Generate a random state value (can be more secure in production)
-    const state = Math.random().toString(36).substring(7);
-  
     // Redirect the user to the Spotify Accounts service for authorization
     res.redirect(`https://accounts.spotify.com/authorize?${querystring.stringify({
       response_type: 'code',
       client_id: CLIENT_ID,
       scope,
       redirect_uri: REDIRECT_URI,
-      state,
     })}`);
   });
   
   app.get('/callback', async (req, res) => {
+    console.log("callback page reached");
     const { code, state } = req.query;
-  
     // Verify the state parameter to prevent CSRF attacks
-    if (state !== 'your-random-state') {
+    /*
+    if (state !== req.session.state) {
       return res.status(400).send('Invalid state parameter');
     }
-  
+    */
     try {
       // Exchange the authorization code for an access token
       const tokenResponse = await fetch('https://accounts.spotify.com/api/token', {
@@ -58,7 +55,7 @@ app.get('/login', (req, res) => {
       accessToken = tokenData.access_token;
   
       // Redirect to a new URL where the user's top songs are displayed
-      res.redirect('/top-songs');
+      res.redirect('./top-songs');
     } catch (error) {
       console.error('Error:', error.message);
       res.status(500).send('Error obtaining access token');
@@ -66,6 +63,7 @@ app.get('/login', (req, res) => {
   });
   
   app.get('/top-songs', async (req, res) => {
+    console.log("Top songs reached");
     try {
       // Fetch the user's top songs using the access token
       const response = await fetch(`${SPOTIFY_API_BASE}/me/top/tracks`, {
@@ -79,6 +77,7 @@ app.get('/login', (req, res) => {
       }
   
       const topSongs = await response.json();
+      console.log(`top songs json = ${topSongs}`);
   
       // Display the user's top songs
       res.send(`<h1>Your Top Songs on Spotify</h1><pre>${JSON.stringify(topSongs, null, 2)}</pre>`);
