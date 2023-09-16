df29859215a0eb85bc9097a0852f1165fa2adf95
Tyler-Culp <tculp@ucsd.edu>
2023-09-16 01:59:22 -0700

    Refining incoming data

--- spotifyProject/app.mjs
+++ spotifyProject/app.mjs
@@ -83,7 +83,7 @@ app.get('/callback', async (req, res) => {
   
       let topSongs = await response.json();
       let list = "<ul>";
-      topSongs = topSongs.items[0];
+      topSongs = topSongs.items[0].name;
       // topSongs = topSongs.items[0].artists[0].name; // This is the way to get the name of each artist from the list of 'items' in the JSON
       console.log(`top songs json = ${topSongs}`);
 
