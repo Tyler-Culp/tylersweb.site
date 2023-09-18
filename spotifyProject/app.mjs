5378c0df874adfe052e5a7296e6561612af924ef
Tyler-Culp <tculp@ucsd.edu>
2023-09-17 23:44:13 -0700

    cleaned up file

--- spotifyProject/app.mjs
+++ spotifyProject/app.mjs
@@ -1,29 +1,12 @@
 import express from 'express';
 import querystring from 'querystring';
 import fetch from 'node-fetch';
-import cors from 'cors'
 import { CLIENT_ID, CLIENT_SECRET, REDIRECT_URI } from './config.mjs';
 
 
 const app = express();
 
-const corsOptions = {
-  origin: 'https://spotify.com', // Allow requests from this domain
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed HTTP methods
-  credentials: true, // Allow cookies and credentials to be sent
-};
-
-app.use((req, res, next) => {
-  console.log("In app.use");
-  res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
-  res.setHeader('Pragma', 'no-cache');
-  res.setHeader('Expires', '0');
-  next();
-});
-app.use(cors(corsOptions));
-
 const PORT = 3000;
-
 
 const SPOTIFY_API_BASE = 'https://api.spotify.com/v1';
 
@@ -32,9 +15,6 @@ app.get('/login', (req, res) => {
   console.log("login page reached");
   const scope = 'user-top-read'; // Specify required scope for top songs
 
-  // Generate a random state value (can be more secure in production)
-  // Redirect the user to the Spotify Accounts service for authorization
-  res.setHeader('Cache-Control', 'no-store');
   res.redirect(`https://accounts.spotify.com/authorize?${querystring.stringify({
     response_type: 'code',
     client_id: CLIENT_ID,
@@ -102,18 +82,6 @@ app.get('/callback', async (req, res) => {
                 <button onclick="logout()">Log Out Here</button>
                 <script>
                   async function logout() {
-                    // try {
-                    //   let resp = await fetch("https://www.spotify.com/logout/");
-                    //   if (resp.status === 200) {
-                    //     window.location.href = "https://tylersweb.site/spotifyProject/login";
-                    //   }
-                    //   else{
-                    //     console.log("Responded with " + response.status);
-                    //   }
-                    // }
-                    // catch (err) {
-                    //   console.error("Fetch request error: " + err);
-                    // }
                     let tempWindow = ""
                     await setTimeout(() => {
                       tempWindow = window.open("https://www.spotify.com/logout/","_blank");
@@ -133,21 +101,6 @@ app.get('/callback', async (req, res) => {
     res.status(500).send('Error obtaining access token');
   }
 });
-
-// app.get("/logout", async (req, res) => {
-//   fetch("https://www.spotify.com/logout/")
-//   .then(r => {
-//     if (r.status == 200 ) {
-//       res.redirect("https://tylersweb.site/spotifyStuff.html");
-//     }
-//     else {
-//       console.log(`r = ${r}`);
-//     }
-//   })
-//   .catch(err => {
-//     console.log(`logout failed : err = ${err}`);
-//   });
-// });
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
